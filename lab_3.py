e99b1dd7ee147f9d7cf678a8d34831da550dd156
daniel21-meet <daniel21@meet.mit.edu>
2019-07-23 09:58:59 -0400

    Stopping here

--- lab_3.py
+++ lab_3.py
@@ -1,9 +1,15 @@
-daniel = input("hi")
+#daniel = input("hi")
 #daniel = daniel.upper ()
 
-print (daniel)
+#print (daniel)
 #daniel = daniel.lower ()
-print (daniel)
-daniel = daniel.swapcase ()
-print (daniel)
+#print (daniel)
+#daniel = daniel.swapcase ()
+#print (daniel)
 
+
+person_one = input("what is the age of first person?")
+person_two = input("what isthe age of the second person?")
+person_one = int(person_one)
+person_two = int(person_two)
+print (abs(person_two - person_one))
